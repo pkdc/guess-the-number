35c776c11cf49ae87e0344864eb0ca480ace3cb8
pkdc <pkdchoy@gmail.com>
2025-01-09 10:39:18 +0000

    Added Comments

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import random
 
 def guess_number():
     try:
+        # Get the maximum number for the game from the user
         max = int(input("Enter the maximum number for the game: "))
         random_num = random.randint(1, max)
+
+        # User input for guessing the number
         while True:
             try:
                 user_num = int(input(f"Enter your guess between 1 and {max}. To exit the game enter 0: "))
+                # Directing the user based on the guess
                 if user_num == 0:
-                    print("Thank you for playing!")
+                    print("Goodbye, thank you for playing!")
                     break
                 elif user_num == random_num:
                     print("You guessed it right!")
@@ -21,4 +25,5 @@ def guess_number():
                 print("Please enter a valid integer!")
     except ValueError:
         print("Please enter a valid integer for the maximum number!")
+
 guess_number()
